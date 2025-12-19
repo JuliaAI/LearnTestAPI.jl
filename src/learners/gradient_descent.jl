5c212c312c8060fee80408f9a8ca73168217b272
Anthony D. Blaom <anthony.blaom@gmail.com>
2025-12-20 08:53:26 +1300

    improve docstring for (non-included) PerceptronClassifier learner

--- src/learners/gradient_descent.jl
+++ src/learners/gradient_descent.jl
@@ -109,20 +109,24 @@ point predictions with `predict(model, Point(), Xnew)`.
 
 # Warm restart options
 
-    update(model, newdata, :epochs=>n, other_replacements...)
+    update(model, data, replacements...)
 
-If `Δepochs = n - perceptron.epochs` is non-negative, then return an updated model, with
-the weights and bias of the previously learned perceptron used as the starting state in
-new gradient descent updates for `Δepochs` epochs, and using the provided `newdata`
-instead of the previous training data. Any other hyperparaameter `replacements` are also
-adopted. If `Δepochs` is negative or not specified, instead return `fit(learner,
-newdata)`, where `learner=LearnAPI.clone(learner; epochs=n, replacements....)`.
+If `replacements` includes `:epochs=>n` and `Δepochs = n - perceptron.epochs` is
+non-negative, then return an updated model, with the weights and bias of the previously
+learned perceptron used as the starting state in new gradient descent updates for
+`Δepochs` epochs, and using the provided `data`. It is possible that `data` is different
+from previously used training data, but `update_observations` may be more appropriate in
+that case (see below). Any other hyperparameter `replacements` are also adopted. If no
+replacement for :epochs is specified, or `Δepochs` is negative, instead return
+`fit(learner, data)`, where `learner=LearnAPI.clone(learner; replacements....)`.
 
     update_observations(model, newdata, replacements...)
 
 Return an updated model, with the weights and bias of the previously learned perceptron
 used as the starting state in new gradient descent updates. Adopt any specified
-hyperparameter `replacements` (properties of `LearnAPI.learner(model)`).
+hyperparameter `replacements` (properties of `LearnAPI.learner(model)`). Exactly `n` new
+epochs are applied, where `n = model.epochs` unless this explictly changed in
+`replacements`.
 
 """
 PerceptronClassifier(; epochs=50, optimiser=Optimisers.Adam(), rng=Random.default_rng()) =
